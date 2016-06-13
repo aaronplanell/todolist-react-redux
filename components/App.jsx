061bf9953df069d7bcc111d2d2f36375b87f153d
Aaron Planell Lopez <aplanell@meteo.cat>
2016-06-13 10:12:55 +0200

    Add a ToDo in a list

---
+++ components/App.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { addTodo } from '../actions/actions'
+
+import AddTodo from './AddTodo.jsx'
+import TodoList from './TodoList.jsx'
+
+class App extends Component {
+   render() {
+      const { dispatch, visibleTodos } = this.props
+
+      return (
+         <div>
+
+            <AddTodo
+               onAddClick = {text =>
+               dispatch(addTodo(text))}
+            />
+
+            <TodoList todos = {visibleTodos}/>
+
+         </div>
+      )
+   }
+}
+
+function select(state) {
+   return {
+      visibleTodos: state.todos
+   }
+}
+
+export default connect(select)(App)
--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('My Todo List React Redux App');
+import React from 'react'
+
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import App from '../components/App.jsx'
+import todoApp from '../reducers/reducers'
+
+let store = createStore(todoApp)
+
+let rootElement = document.getElementById('app')
+
+render(
+
+   <Provider store = {store}>
+      <App />
+   </Provider>,
+
+   rootElement
+)
