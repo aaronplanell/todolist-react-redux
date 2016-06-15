13ce7b9025b7e4c92e178cb6e2f0cb9ef79bd1ef
Aaron Planell Lopez <aplanell@meteo.cat>
2016-06-15 13:38:38 +0200

    Refactor files/folders

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import { render } from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
-import App from '../components/App.jsx'
-import root from '../reducers/root'
+import App from './components/App.jsx'
+import root from './reducers/root'
 
 let store = createStore(root, window.devToolsExtension ? window.devToolsExtension() : f => f);
 
